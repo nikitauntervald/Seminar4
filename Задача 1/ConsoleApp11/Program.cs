1b9a80c4a67f5bc3e51371f7788f487f2ae633a0
NikitaUnterwald <60911883+nikitauntervald@users.noreply.github.com>
2023-12-05 10:48:41 +0300

    Add files via upload

---
+++ Задача 1/ConsoleApp11/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double res = 0;
+        int NumToSumDigits(int number)
+        {
+            int sum = 0;
+
+            while (number != 0)
+            {
+                sum += number % 10;
+                number /= 10;
+            }
+            return int.Abs(sum);
+        }
+
+        void Loop()
+        {
+            while (true)
+            {
+                Console.Write("Enter integer number or 'q' for exit: ");
+                string str = Console.ReadLine();
+                if (str != null)
+                {
+                    if (str.ToLower() == "q")
+                        break;
+                }
+                int num;
+                if (int.TryParse(str, out num))
+                {
+                    num = NumToSumDigits(num);
+                    if ((num & 0x01) == 0)
+                    {
+                        Console.WriteLine("Sum of digits is even!");
+                        break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Bad number!");
+                }
+            }
+            Console.WriteLine("[STOP]");
+        }
+
+
+        Console.Clear();
+        Loop();
+    }
+}
+
---
+++ Задача 3/ConsoleApp10/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int[] array = GetArray(10, 0, 10);
+        Console.WriteLine(string.Join(" ", array));
+
+        int[] reverseArray = ReversArray1(array);
+        Console.WriteLine(string.Join(" ", reverseArray));
+
+
+        int[] GetArray(int size, int minValue, int maxValue)
+        {
+            int[] res = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                res[i] = new Random().Next(minValue, maxValue + 1);
+            }
+            return res;
+        }
+
+        int[] ReversArray1(int[] inArray)
+        {
+            int[] result = new int[inArray.Length];
+            for (int i = 0; i < inArray.Length; i++)
+            {
+                result[i] = inArray[inArray.Length - 1 - i];
+            }
+            return result;
+        }
+    }
+}
